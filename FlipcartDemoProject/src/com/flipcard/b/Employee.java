36ad51c6388b42e9a44f52df3fe6c377dcd5b4a6
adi <adityaraj791998@gmail.com>
2020-05-30 16:41:36 +0530

    commit with update

---
+++ FlipcartDemoProject/src/com/flipcard/b/Employee.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package com.flipcard.b;
+
+/**
+ * @author Administrator
+ *
+ */
+public class Employee {
+	private int empId;
+	public int getEmpId() {
+		return empId;
+	}
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	private String empName;
+}
--- FlipcartDemoProject/src/com/flipcard/demo/DemoProject.java
+++ FlipcartDemoProject/src/com/flipcard/demo/DemoProject.java
@@ -1,7 +1,11 @@
 package com.flipcard.demo;
 
+import com.flipcard.b.Employee;
+
 public class DemoProject {
 	public static void main(String[] args){
 		System.out.println("demo started");
+		System.out.println("for checking");
 	}
+	
 }
